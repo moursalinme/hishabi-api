9d6fa77645f7c5e4f1fe6d1b6c2bfd237d198d95
moursalinme <me.moursalin@gmail.com>
2024-11-24 20:41:14 +0600

    feat: added user services for getAllUsers, getUserById, getUserByEmail, createUser, deleteUserById- No implementation yet

--- src/main/java/com/hishabi/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/hishabi/api/service/impl/UserServiceImpl.java
@@ -1,15 +1,50 @@
 package com.hishabi.api.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.hishabi.api.dto.request.CreateUserDto;
+import com.hishabi.api.dto.response.UserResponseDto;
 import com.hishabi.api.repository.UserRepository;
+import com.hishabi.api.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+
+    @Override
+    public List<UserResponseDto> getAllUsers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllUsers'");
+    }
+
+    @Override
+    public UserResponseDto getUserById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUserById'");
+    }
+
+    @Override
+    public UserResponseDto getUserByEmail(String email) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUserByEmail'");
+    }
+
+    @Override
+    public UserResponseDto createUser(CreateUserDto user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteUserById'");
+    }
 
 }

8e6dd42d4e81a6c89ce9d30e9f15defe3c36ccd7
moursalinme <me.moursalin@gmail.com>
2024-11-24 20:48:46 +0600

    update: added builder pattern

--- src/main/java/com/hishabi/api/dto/response/UserResponseDto.java
+++ src/main/java/com/hishabi/api/dto/response/UserResponseDto.java
@@ -3,12 +3,14 @@ package com.hishabi.api.dto.response;
 import java.util.Date;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserResponseDto {
 
     private Long id;

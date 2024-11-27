feeb2a654b0bbe1ad075a850daca1226d1644778
moursalinme <me.moursalin@gmail.com>
2024-11-27 18:51:58 +0600

    feat: created Income entity

--- src/main/java/com/hishabi/api/entity/IncomeEntity.java
+++ src/main/java/com/hishabi/api/entity/IncomeEntity.java
@@ -1,11 +1,11 @@
 package com.hishabi.api.entity;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -14,7 +14,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,38 +21,32 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "month_table")
-public class MonthEntity {
+@Builder
+@Table(name = "income_table")
+public class IncomeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private UserEntity user;
+    @JoinColumn(name = "month_id", nullable = false)
+    private MonthEntity month;
 
     @Column(nullable = false)
-    private Integer month;
+    private String source;
 
     @Column(nullable = false)
-    private Integer year;
-
-    @Column(nullable = false)
-    private Double balance;
+    private BigDecimal amount;
 
     @CreationTimestamp
-    @Column(updatable = false)
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     @Column(nullable = false)
     private LocalDateTime updatedAt;
-
-    @OneToMany(mappedBy = "month", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private List<IncomeEntity> incomes;
-
 }

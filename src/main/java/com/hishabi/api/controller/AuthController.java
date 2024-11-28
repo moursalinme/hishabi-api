4313d9f77b25aad1cb38cb366b3c74846ba9c7d1
moursalinme <me.moursalin@gmail.com>
2024-11-28 20:09:42 +0600

    feat: Added docs in Auth controller

--- src/main/java/com/hishabi/api/controller/AuthController.java
+++ src/main/java/com/hishabi/api/controller/AuthController.java
@@ -20,18 +20,28 @@ import com.hishabi.api.dto.response.RegistrationResponse;
 import com.hishabi.api.dto.response.UserResponseDto;
 import com.hishabi.api.service.AuthService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Tag(name = "Authentication", description = "APIs for user registration and login")
 public class AuthController {
 
     private final AuthService authService;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
 
+    /**
+     * Register a new user.
+     *
+     * @param user The user data provided in the request body.
+     * @return A ResponseEntity containing the created user details and a JWT token.
+     */
+    @Operation(summary = "Register a new user", description = "This endpoint allows a user to register and returns their details along with a JWT token.")
     @PostMapping("/register")
     public ResponseEntity<ApiResponse<RegistrationResponse>> registerUser(@RequestBody @Valid UserRequestDto user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -45,6 +55,14 @@ public class AuthController {
         return ApiResponse.success(201, response);
     }
 
+    /**
+     * Authenticate a user and return a JWT token.
+     *
+     * @param loginReq The login credentials provided in the request body.
+     * @return A ResponseEntity containing the JWT token and user details upon
+     *         successful login.
+     */
+    @Operation(summary = "Login a user", description = "This endpoint authenticates a user and returns a JWT token along with their details.")
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<Object>> handleLogin(@RequestBody @Valid LoginRequestDto loginReq) {
         UserResponseDto user;
